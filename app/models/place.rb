b13655d34717d3f6a4137ba6f5e363f98d3865a8
Guibibi <bgui34@gmail.com>
2021-05-06 18:13:05 -0400

    Added validation

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,10 @@
 class Place < ApplicationRecord
     has_many :place_tags
     has_many :tags, through: :place_tags
+    validates :name, presence: true
+    validates :lat, presence: true
+    validates :long, presence: true
+    validates :zip, presence: true
+    validates :address, presence: true
+    validates :country, presence: true
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,5 @@
 class Tag < ApplicationRecord
   has_many :place_tags
   has_many :places, through: :place_tags
+  validates :keyword, presence: true
 end
